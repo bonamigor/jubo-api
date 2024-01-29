0ca7dc1fe000e17bd253d8ec008456ca0ba178be
Rafael Ribeiro Bonamigo <rafaelrbonamigo@gmail.com>
2024-01-28 22:57:16 -0300

    Duplicacao de estantes

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const app = require('./src/index');
 
 const port = process.env.PORT || '8080';
 
+const estanteController = require('./src/controllers/estantes.controller');
+
+estanteController.selecionarClientes();
+// estanteController.selecionarEstantes();
+
+setTimeout(() => {
+  estanteController.selecionarEstantes();
+}, 10000);
+
 app.listen(port, () => {
   console.log('Aplicação sendo executada na porta:', port);
 });
