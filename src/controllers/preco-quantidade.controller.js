7947c4cf740c00a4081abad4b0f8de79ed49447b
Rafael Ribeiro Bonamigo <rafaelrbonamigo@gmail.com>
2024-12-23 10:30:06 -0300

    Adicionado validacao para nao recuperar produtos desativados

--- src/controllers/preco-quantidade.controller.js
+++ src/controllers/preco-quantidade.controller.js
@@ -47,7 +47,7 @@ exports.listarDetalhadoNoPedido = async (req, res) => {
     INNER JOIN produtos ON estante_produto.produto_id = produtos.id
     INNER JOIN preco_quantidade ON estante_produto.preco_quantidade_id = preco_quantidade.id
     WHERE estante_produto.estante_id = ? 
-    AND estante_produto.ativo = 1`;
+    AND estante_produto.ativo = 1 AND produtos.ativo = 1`;
 
     db.execute(selectQuery, [req.params.id], (err, results) => {
       if (err) {
