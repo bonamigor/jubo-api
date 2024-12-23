572419abe33b28f23fcdce37b44707d97d52b6c9
Rafael Bonamigo <rafaelrbonamigo@gmail.com>
2024-12-23 11:09:29 -0300

    Criação método q recupera todos os itens independente de status durante cadastro de produto

--- src/routes/produtos.routes.js
+++ src/routes/produtos.routes.js
@@ -6,6 +6,9 @@ const produtoController = require('../controllers/produtos.controller');
 // ==> GET http://localhost:3000/api/produtos
 router.get('/produtos', produtoController.listAllProdutos);
 
+// ==> GET http://localhost:3000/api/produtos
+router.get('/produtos/admin', produtoController.listAllProdutosParaAdmin);
+
 // ==> GET http://localhost:3000/api/produtos/id
 router.get('/produtos/:id', produtoController.listOneProduto);
 
