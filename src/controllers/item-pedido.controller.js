bcd1c3adbbb109029f8b71aa4a15bca4e7e345e8
Rafael Bonamigo <rafaelrbonamigo@gmail.com>
2025-01-24 19:41:29 -0300

    Regra de quebra de linha desligada e limpeza de espaço.
    A regra de quebra de linha foi desativada pois o desenvolvimento do sistema pode acontecer em sistemas operacionais diferentes;

--- src/controllers/item-pedido.controller.js
+++ src/controllers/item-pedido.controller.js
@@ -112,7 +112,7 @@ exports.deletarItemPedido = async (req, res) => {
       itemPedidoId, estanteId, produtoId,
     } = req.params;
     const deleteQuery = `DELETE FROM item_pedido where item_pedido.id = ${itemPedidoId}`;
-    
+
     db.query(deleteQuery, (error, results) => {
       if (error) {
         res.status(500).send({
